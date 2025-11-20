ad115a85943f972a9b9d6e077232c049d476f101
Sofy Moncada <qmoncadasofia@gmail.com>
2025-11-20 11:44:24 -0300

    intenté hacer el main

--- enemigos.py
+++ enemigos.py
@@ -1,19 +1,21 @@
 from personaje import personaje
 
 class bowser:
-    def __init__(self, nombre, vida):
+    def __init__(self, nombre, vida, poderes):
+        super().__init__(nombre, vida)
         self.__nombre = nombre
         self.__vida = []
+        self.__poderes = poderes
 
     def lanzar_fuego(self):
-        return f"{self.nombre} lanza  fuego!"
+        return f"{self.nombre} lanzó  fuego!"
 
     def recibir_daño(self, daño):
         self.vida -= daño
         return f"{self.nombre} recibió {daño} de daño y le quedan {self.vida} de vida."
     
     def ataque_con_salto (self):
-        return f"{self.__nombre} ataca con un salto!"
+        return f"{self.__nombre} atacó con un salto!"
     
     def combo_ataque (self):
-        return f"{self.__nombre} realiza un combo de ataques!"
+        return f"{self.__nombre} realizó un combo de ataques!"
---
+++ main.py
@@ -0,0 +1,19 @@
+from mario import mario
+from luigi import luigi
+from enemigos import bowser
+
+def main():
+
+    while True:
+        print("MENÚ DE OPCIONES")
+        print("1. Mario come un honguito")
+        print("2. Mario come una flor")
+        print("3. Luigi come un honguito")
+        print("4. Luigi come una flor")
+        print("5. Bowser lanza fuego")
+        print("6. Bowser realiza ataque con salto")
+        print("7. Bowser realiza un combo de ataque")
+        print("8. Bowser recibe daño")
+        print("0. Salir")
+
+        opcion = input("selecciona una opción: ")
